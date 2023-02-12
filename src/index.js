83cef2c27dbedb53a6deb6f01ae6a6ddc00ac324
sosajseba <sosajseba@gmail.com>
2023-02-12 19:56:39 -0300

    feat: join room and create room

--- src/index.js
+++ src/index.js
@@ -15,6 +15,37 @@ const io = new Server(server, {
     }
 })
 
+var room = {};
+
+io.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`)
+
+    socket.on('create-room', (data) => {
+        console.log(data);
+        room = data;
+        socket.join(data.roomId);
+    })
+
+    socket.on('join-room', (data) => {
+        console.log(room.players);
+        if (room.players) {
+            if (room.players.length < 3) //TODO: move max playersto an env variable
+            {
+                socket.join(data.roomId);
+                room.players.push(data.player);
+                socket.nsp.to(data.roomId).emit('new-player', room);
+            } else {
+                socket.nsp.to(data.player.id).emit('room-is-full', true);
+            }
+        }
+    })
+
+    socket.on('send', (data) => {
+        console.log(data);
+        socket.to(data.room).emit('receive', data.message)
+    })
+})
+
 server.listen(3001, () => {
     console.log('Server is running on port 3001')
 })
